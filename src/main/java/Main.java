9d95d26d960314b40fbde86b0bd7d6e340af06d3
nigel <bynum1001@gmail.com>
2023-07-02 19:53:11 -0400

    Completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Objects;
+import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args ){
 
-    }
-}
+    public static void main(String[] args ) {
+        // his or her name to equal alice and bob
+        // I need it to greet alice and bob
+        String user;
+
+        System.out.println("Insert Name: ");
+        Scanner nigel = new Scanner(System.in);
+        user = nigel.nextLine();
+
+
+        if (Objects.equals(user, "Alice") || Objects.equals(user, "Bob")) {
+            System.out.println("Greetings " + user);
+
+        }
+    }}
